42d0ff3e419148a7d7dd9187ba412973ba79d2d5
OlgaMigal <111079530+OlgaMigal@users.noreply.github.com>
2023-09-26 01:23:38 +0300

    Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов

--- src/homework/hometask6/task1/PhoneBook.java
+++ src/homework/hometask6/task1/PhoneBook.java
@@ -1,76 +1,63 @@
-package homework.hometask6.task1and2;
-
-//import java.io.BufferedReader;
-import java.io.*;
-//import java.io.FileWriter;
-//import java.io.IOException;
-import java.lang.Exception;
+package homework.hometask6.task1;
+import java.util.*;
 
 /*
 1. Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
-2. Пусть дан список сотрудников:Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова,
-Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова,
-Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов.
-
-Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
-Отсортировать по убыванию популярности. Для сортировки можно использовать TreeMap, мы не успели,
-но я расписал пример использования и прикрепил к материалам урока в файле TestTreeMap
-
-
-
  */
-
-
 public class PhoneBook {
-    public static void main(String[] args) throws Exception{
-        fileCreator();
-        fileReader();
-//        File file = new File("/Users/anteya/IdeaProjects/Projects/src/homework/hometask6/task1and2/PhoneBook.java/file");
-//        if (file.createNewFile()) {
-//            System.out.println("Файл создан");
-//        } else {
-//            System.out.println("Проблемка и не создан");
-//        }
-
-
-    }
-    public static void fileCreator(){
-        try (FileWriter writer = new FileWriter("file.txt", false)) {
-            String text = "TEST TEST TEST HELP ME";
-            writer.write(text);
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        public static int getChoice() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("1 - Добавление контакта \n2 - Вывод всех контактов \n3 - Поиск по фамилии \n4 - Выход");
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case ("1"):
+                    return 1;
+                case ("2"):
+                    return 2;
+                case ("3"):
+                    return 3;
+                case ("4"):
+                    return 4;
+            }
+            return 0;
         }
+        public static void actions() {
+            Scanner scanner = new Scanner(System.in);
+            Map<String, ArrayList<String>> phoneBook = new HashMap<>();
+            add: while (true) {
+                int choice = getChoice();
+                if (choice == 1) {
+                    System.out.print("Введите имя: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Введите номер телефона: ");
+                    String number = scanner.nextLine();
+                    if (!phoneBook.containsKey(name)) {
+                        ArrayList<String> numbers = new ArrayList<>();
+                        numbers.add(number);
+                        phoneBook.put(name, numbers);
+                    } else {
+                        var numbers = phoneBook.get(name);
+                        numbers.add(number);
+                        phoneBook.put(name, numbers);
+                    }
+                }
+                if (choice == 2) {
+                    System.out.println(phoneBook);;
+                }
+                if (choice == 3) {
+                    System.out.println("Введите имя для поиска: ");
+                    String name = scanner.nextLine();
+                    System.out.println(phoneBook.get(name));
+                }
+                if (choice == 4) {
+                    break;
+                }
+            }
+            scanner.close();
+        }
+        public static void main(String[] args) {
+            actions();
+
+
     }
-     public static void fileReader() throws Exception{
-         BufferedReader br = new BufferedReader(new FileReader("file.txt"));
-         String str;
-         while ((str = br.readLine())!=null){
-             System.out.printf("== %s == \n", str);
-         }
-         br.close();
-     }
 }
-//
-//
-//
-//    }
-//
-//    public void fileCreator() throws IOException {
-//
-//
-//}
-//
-//
-//class Interface{
-//
-//}
-//
-//class ActionsWithPhoneBook{
-//
-//}
-
-
-
-
